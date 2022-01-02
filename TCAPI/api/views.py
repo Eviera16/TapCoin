d1ddf6450f3ed66bcdf657ddb1846293c8cfa705
Eviera16 <64331763+Eviera16@users.noreply.github.com>
2022-01-01 22:10:14 -0500

    adjusting game view

--- TCAPI/api/views.py
+++ TCAPI/api/views.py
@@ -73,6 +73,9 @@ def get_user(request):
     
     serializer = GetUserSerializer(data=newData)
 
+    queue = Queue.objects.get(queueId=config('QUEUEID', cast=int))
+    queue.delete()
+
     data = {}
 
     if serializer.is_valid():
