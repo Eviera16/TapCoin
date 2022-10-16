16ae005e9757091652df2e00e7c5e06c2b41b7e6
Eviera16 <64331763+Eviera16@users.noreply.github.com>
2022-10-16 17:59:54 -0400

    optional phone number field/email settings comments

--- TCAPI/api/views.py
+++ TCAPI/api/views.py
@@ -34,6 +34,8 @@ def registration_view(request):
             user1.in_game = False
             user1.in_queue = False
             user1.logged_in = True
+            if user1.phone_number != "":
+                user1.has_phone_number = True
             user1.save()
         else:
             data = serializer.errors
@@ -104,6 +106,8 @@ def get_user(request):
                 invites.append(invite.sender)
         data['hasInvite'] = hasInvites
         data['invites'] = invites
+        if request.data['from'] == "Home":
+            data['HPN'] = user.has_phone_number
     else: 
         data = serializer.errors
     return Response(data)
--- TCServer/settings.py
+++ TCServer/settings.py
@@ -158,3 +158,10 @@ if os.getcwd() == '/app':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SECURE_SSL_REDIRECT = True
 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = '587'
+# EMAIL_HOST_USER = 'thundervieraeric@gmail.com'
+# EMAIL_HOST_PASSWORD = '1999116eric'
+# EMAIL_USE_TLS = True
+
