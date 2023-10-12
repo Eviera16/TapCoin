ee3b73f81e19a00e2ca5721f419d536442f4d4e8
Eviera16 <64331763+Eviera16@users.noreply.github.com>
2023-10-12 07:39:45 -0400

    changes to settings db

--- TCServer/settings.py
+++ TCServer/settings.py
@@ -96,10 +96,10 @@ WSGI_APPLICATION = 'TCServer.wsgi.application'
 DATABASES = {
     'default' : {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
+        'NAME': config('DEV_DATABASE_NAME') if DEBUG else config('DATABASE_NAME'),
+        'USER': config('DEV_DATABASE_USER') if DEBUG else config('DATABASE_USER'),
+        'PASSWORD': config('DEV_DATABASE_PASSWORD') if DEBUG else config('DATABASE_PASSWORD'),
+        'HOST': config('DEV_DATABASE_HOST') if DEBUG else config('DATABASE_HOST'),
         'PORT': '5432',
     }
 }
